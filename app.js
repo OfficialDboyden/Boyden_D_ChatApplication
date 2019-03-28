e5377b8ec010f23fc089ac532454d86bad25786f
OfficialDboyden <36012755+OfficialDboyden@users.noreply.github.com>
2019-03-28 17:10:03 -0400

    done
    done

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const io = require('socket.io')();
 
 const port = process.env.PORT || 3000;
 
@@ -12,6 +11,23 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
-http.listen(port, () => {
-    console.log(`app is running on port ${port}`);
+const server = app.listen(port, () => {
+  console.log(`app is running on port ${port}`);
+});
+
+io.attach(server);
+
+io.on('connection', function(socket){
+  console.log('a user has connected');
+
+socket.emit('connected', {sID:`${socket.id}`, message: 'new connection'} );
+socket.on('chat message', function (msg){
+  console.log('message', msg, 'socket', socket.id);
+
+  io.emit('chat message', {id: `${socket.id}`,message: msg});
+})
+  socket.on('disconnect', function() {
+
+    console.log('a user has diconnected');
+  });
 });
